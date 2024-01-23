bd36ef9b4608228179de7064e466be607641aa34
DEVANSH TYAGI <96236567+Devansh8954@users.noreply.github.com>
2024-01-23 22:08:28 +0530

    Create Ransom-Note.cpp

---
+++ Ransom-Note.cpp
@@ -0,0 +1,54 @@
+/*
+383. Ransom Note
+Solved
+Easy
+Topics
+Companies
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+*/
+auto fastio=[](){
+std::ios::sync_with_stdio(false);
+cin.tie(nullptr);
+cout.tie(nullptr);
+return nullptr;
+}();
+
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        int freq[26] = {0};
+
+        for(auto x: magazine){
+            freq[x - 97] ++;
+        }
+
+        for(auto x: ransomNote){
+            if(freq[x - 97] <= 0){
+                return false;
+            }
+            freq[x - 97] --;
+        }
+
+        return true;
+        
+    }
+};
