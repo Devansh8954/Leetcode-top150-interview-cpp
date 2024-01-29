e195178a5d1e6447c8f6a89f4ec21f144f712162
DEVANSH TYAGI <96236567+Devansh8954@users.noreply.github.com>
2024-01-29 22:20:10 +0530

    Update Happy-Number.cpp

--- Happy-Number.cpp
+++ Happy-Number.cpp
@@ -56,3 +56,23 @@ public:
     }
 };
 */
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_map<int,int> mp;
+        while(n != 1){
+            if(mp[n] > 1) return false;
+
+            mp[n]++;
+            int sum=0;
+            while(n > 0){
+                int val = n%10;
+                sum += val*val;
+                n = n/10;
+            }
+            n=sum;
+        }
+        return true;
+    }
+};
