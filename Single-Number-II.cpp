c1d41a52b576750f6f4d3a7b281e66ee6f960891
DEVANSH TYAGI <96236567+Devansh8954@users.noreply.github.com>
2024-02-08 21:18:35 +0530

    Update Single-Number-II.cpp

--- Single-Number-II.cpp
+++ Single-Number-II.cpp
@@ -45,5 +45,18 @@ public:
         return -1;
     }
 };
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0;
+        int twos = 0;
 
+        for (const int num : nums) {
+            ones ^= (num & ~twos);
+            twos ^= (num & ~ones);
+        }
+
+        return ones;
+    }
+};
 */
