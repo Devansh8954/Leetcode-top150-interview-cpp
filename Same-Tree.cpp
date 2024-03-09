6f4eb9af0b75e86899c966188dacb44df974a86f
DEVANSH TYAGI <96236567+Devansh8954@users.noreply.github.com>
2024-03-09 16:51:42 +0530

    Update Same-Tree.cpp

--- Same-Tree.cpp
+++ Same-Tree.cpp
@@ -43,7 +43,7 @@ The number of nodes in both trees is in the range [0, 100].
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+ *
 class Solution {
 public:
     bool solve(TreeNode* p, TreeNode* q){
@@ -86,6 +86,28 @@ public:
             }
         }
         return true;
+    }
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return solve(p,q);
+    }
+};
+*/
+
+class Solution {
+public:
+    bool solve(TreeNode* p, TreeNode* q){
+        if(!p && !q){ // p and q both NULL
+            return true;
+        }
+        if(!p || !q){ // Any 1 null
+            return false;
+        }
+        if(p->val != q->val){
+            return false;
+        }
+
+        return (solve(p->left,q->left) && solve(p->right,q->right));
     }
 
     bool isSameTree(TreeNode* p, TreeNode* q) {
